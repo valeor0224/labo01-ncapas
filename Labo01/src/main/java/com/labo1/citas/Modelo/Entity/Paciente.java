7103cf9b701f2a9f56d2206ee06abc6dab5bcce5
valeor0224 <00206022@uca.edu.sv>
2025-04-02 00:03:57 -0600

    Cambio variable fecha

--- Labo01/src/main/java/com/labo1/citas/Modelo/Entity/Paciente.java
+++ Labo01/src/main/java/com/labo1/citas/Modelo/Entity/Paciente.java
@@ -1,10 +1,12 @@
 package com.labo1.citas.Modelo.Entity;
 
+import java.time.LocalDateTime;
+
 public class Paciente extends Persona {
 
     //private Persona persona;
 
-    public Paciente(String nombre, String apellido, String DUI, String fechaNacimiento) {
+    public Paciente(String nombre, String apellido, String DUI, LocalDateTime fechaNacimiento) {
         super(nombre, apellido, DUI, fechaNacimiento);
     }
 }
